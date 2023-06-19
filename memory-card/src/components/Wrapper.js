309003127bdf9c6d41ec39fba839b73640296f16
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-06-19 16:06:54 +0200

    Add Wrapper

---
+++ memory-card/src/components/Wrapper.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import Card from "./Card";
+
+export default function Wrapper({villagers, onCardClick}) {
+
+    const villagerCards = villagers.map((villager) => (
+        <Card key={villager.id} villager={villager} onCardClick={onCardClick}/>
+    ))
+
+    return(
+        <div className="wrapper">
+            {villagerCards}
+        </div>
+    )
+}
